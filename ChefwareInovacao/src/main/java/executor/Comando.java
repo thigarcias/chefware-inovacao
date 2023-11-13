07094a6fe83eaad42c262c5d4818032ca6421c6b
Thiago <thiago.romaris@sptech.school>
2023-11-13 10:09:17 -0300

    Updating API functions

--- ChefwareInovacao/src/main/java/executor/Comando.java
+++ ChefwareInovacao/src/main/java/executor/Comando.java
@@ -1,6 +1,7 @@
 package executor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,17 @@ import java.net.Socket;
 @RestController
 public class Comando {
     @PostMapping("/inovacao")
+    @CrossOrigin
     public ResponseEntity<String> bloquear(@RequestBody Chefware comando) throws IOException {
         Socket socket = new Socket("localhost", 4000);
-        if (comando.getComando() == null || comando.isEmpty()) {
+        if (comando.getComando() == null || comando.getComando().isEmpty()) {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println("O comando não pode ser nulo ou vazio.");
+            return ResponseEntity.badRequest().body("O comando não pode ser nulo ou vazio.");
         }
+
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         out.println(comando.getComando());
-        return ResponseEntity.ok("Comando %s executado com sucessos!".formatted(comando.getComando()));
+        return ResponseEntity.ok("Comando %s executado com sucesso!".formatted(comando.getComando()));
     }
 }
-
-
